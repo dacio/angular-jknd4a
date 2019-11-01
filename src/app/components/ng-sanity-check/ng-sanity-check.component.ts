57e88225587cb225381a482e8f729276e672cb0f
Jose Dacio <jose.dacio@zayo.com>
2019-10-31 22:25:58 -0700

    ngSanityCheck component

---
+++ src/app/components/ng-sanity-check/ng-sanity-check.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-sanity-check',
+  templateUrl: './ng-sanity-check.component.html',
+  styleUrls: ['./ng-sanity-check.component.scss']
+})
+export class NgSanityCheckComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
